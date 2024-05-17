da2319789f92a1f13cf0b5856b13ad9c65b3b802
DucDai <phducdai47@gmail.com>
2024-05-17 19:21:59 +0700

    Added `@GET("v1/notifications")` endpoint in `UserAPI` interface

--- app/src/main/java/com/example/truyenapp/api/UserAPI.java
+++ app/src/main/java/com/example/truyenapp/api/UserAPI.java
@@ -1,11 +1,14 @@
 package com.example.truyenapp.api;
 
 import com.example.truyenapp.model.APIResponse;
+import com.example.truyenapp.model.Notification;
 import com.example.truyenapp.request.ChangePasswordRequest;
 import com.example.truyenapp.request.ForgotPasswordRequest;
 import com.example.truyenapp.request.UserRequest;
 import com.example.truyenapp.response.AttendanceResponse;
 import com.example.truyenapp.response.UserResponse;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -28,4 +31,7 @@ public interface UserAPI {
 
     @POST("v1/attendance")
     Call<APIResponse<AttendanceResponse>> attendance();
+
+    @GET("v1/notifications")
+    Call<APIResponse<ArrayList<Notification>>> getNotifications();
 }
