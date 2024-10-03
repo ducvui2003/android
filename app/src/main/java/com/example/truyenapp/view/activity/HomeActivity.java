5732e940f5abd471b931a1cdd61a6d81981c256a
Le Anh Duc <ducvui2003@gmail.com>
2024-10-03 14:38:03 +0700

    fix: bottom nav

--- app/src/main/java/com/example/truyenapp/view/activity/HomeActivity.java
+++ app/src/main/java/com/example/truyenapp/view/activity/HomeActivity.java
@@ -50,7 +50,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
 //        initNavigateBottom();
 ////        getNumberNotifications();
